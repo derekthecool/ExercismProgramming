8fabe2f22d7f4b1b62663365d196de1c6ee3e88c
Derek Lomax <derekthecool@gmail.com>
2023-06-27 11:07:33 -0600

    Lua hamming first try complete

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,7 +1,20 @@
 local Hamming = {}
 
-function Hamming.compute(a,b)
-     return 0
+function Hamming.compute(a, b)
+	local length_a = #a
+	if length_a ~= #b then
+		return -1
+	elseif length_a == 0 or a == b then
+		return 0
+	else
+		local hamming = 0
+		for i = 1, length_a + 1 do
+			if a:sub(i, i) ~= b:sub(i, i) then
+				hamming = hamming + 1
+			end
+		end
+		return hamming
+	end
 end
 
 return Hamming
