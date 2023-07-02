dd16e66e14d0a835aaabd39f2d0730ed367aa1e8
Derek Lomax <derekthecool@gmail.com>
2023-07-01 22:15:11 -0600

    Edit gitignore and add a few more exercises

---
+++ lua/house/house.lua
@@ -0,0 +1,18 @@
+local house = {}
+
+local is_the = {
+	[[the house that Jack built.]],
+	[[malt]],
+	[[rat]],
+	[[cat]],
+	[[dog]],
+	[[cow with the crumplet horn]],
+	[[maiden all forlorn]],
+
+}
+
+house.verse = function(which) end
+
+house.recite = function() end
+
+return house
