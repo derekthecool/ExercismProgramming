238360f46be475c25161e52077288a18d6073dca
Derek <derekthecool@gmail.com>
2023-07-02 13:43:15 -0600

    Add lua difference of squares

---
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -0,0 +1,18 @@
+local function square_of_sum(n)
+return  6
+end
+
+local function sum_of_squares(n)
+return  5
+end
+
+local function difference_of_squares(n)
+ print(_G.version)
+        return 7
+end
+
+return {
+  square_of_sum = square_of_sum,
+  sum_of_squares = sum_of_squares,
+  difference_of_squares = difference_of_squares
+}
