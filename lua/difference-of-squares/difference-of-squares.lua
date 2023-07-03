083475acd9124195b9dc48454f145ccc33346acd
Derek <derekthecool@gmail.com>
2023-07-03 07:45:56 -0600

    Complete lua difference of squares first time in lua
    This exercise is very great for speed programming. More work needed!

--- lua/difference-of-squares/difference-of-squares.lua
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -1,18 +1,23 @@
-local function square_of_sum(n)
-return  6
+local M = {}
+
+M.square_of_sum = function(n)
+	local output = 0
+	for i = 1, n do
+		output = i + output
+	end
+	return output * output
 end
 
-local function sum_of_squares(n)
-return  5
+M.sum_of_squares = function(n)
+    local output = 0
+	for i = 1, n do
+		output = i * i + output
+	end
+	return output
 end
 
-local function difference_of_squares(n)
- print(_G.version)
-        return 7
+M.difference_of_squares = function(n)
+	return (M.square_of_sum(n) - M.sum_of_squares(n))
 end
 
-return {
-  square_of_sum = square_of_sum,
-  sum_of_squares = sum_of_squares,
-  difference_of_squares = difference_of_squares
-}
+return M
