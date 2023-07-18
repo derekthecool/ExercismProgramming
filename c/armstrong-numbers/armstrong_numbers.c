b71049bba2c4a113b16ed9bfdc02988d752b559a
Derek Lomax <derekthecool@gmail.com>
2023-07-18 10:04:48 -0600

    feat: Add basic starter to a C code example
    requires podman/docker to run on windows

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,1 +1,12 @@
 #include "armstrong_numbers.h"
+#include <stdio.h>
+
+// I could not get the build to work on windows
+// So a used podman run -it --rm -v ${pwd}:/test ubuntu
+// # apt update && apt install -y build-essential
+// # make
+
+bool is_armstrong_number(int candidate)
+{
+     return candidate == 0;
+}
