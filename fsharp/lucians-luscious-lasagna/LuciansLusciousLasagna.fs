f41626cb247cbc9a0c36c98210a0e6dfda4d4e68
Derek Lomax <derekthecool@gmail.com>
2023-08-02 13:52:14 -0600

    Add fsharp lasagna

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,13 @@
+module LuciansLusciousLasagna
+
+let expectedMinutesInOven =
+    0
+
+let remainingMinutesInOven minutes =
+    0
+
+let preparationTimeInMinutes layers =
+    0
+
+let elapsedTimeInMinutes layers minutesDone =
+    0
