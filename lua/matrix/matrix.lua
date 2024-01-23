1509330097fe229d05f113a48b8fda6f2f150f64
Derek Lomax <derekthecool@gmail.com>
2024-01-23 13:56:25 -0700

    Finished matrix

--- lua/matrix/matrix.lua
+++ lua/matrix/matrix.lua
@@ -1,11 +1,26 @@
 return function(s)
 	local M = {}
 
-	M.row = function(a)
-		return { 0, 0, 0 }
+	local matrix = {}
+	local count = 0
+	for row in s:gmatch("[^\n]+") do
+		count = count + 1
+		matrix[count] = {}
+		for column in row:gmatch("%d+") do
+			table.insert(matrix[count], tonumber(column))
+		end
 	end
+
+	M.row = function(a)
+		return matrix[a]
+	end
+
 	M.column = function(a)
-		return { 0, 0, 0 }
+		local column_output = {}
+		for i = 1, #matrix do
+			table.insert(column_output, matrix[i][a])
+		end
+		return column_output
 	end
 
 	return M
