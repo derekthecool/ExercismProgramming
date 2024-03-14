235e6248a77bef93f45c301474b4f9851ce63c68
Derek Lomax <derekthecool@gmail.com>
2024-03-14 11:50:30 -0600

    Finish acronym powershell

--- powershell/acronym/Acronym.ps1
+++ powershell/acronym/Acronym.ps1
@@ -17,8 +17,25 @@ Function Get-Acronym() {
     Param (
         [string]$Phrase
     )
-    return [System.Text.RegularExpressions.Regex]::Matches($Phrase.ToUpper(), "\b[_]*([a-zA-Z])") |
-    ForEach-Object { $_.Groups[1].Value } |
-    Join-String -Separator ""
+    # Create the dotnet regex inside a powershell here-string
+    # A here-string starts with @" and ends with "@ - and the final terminator MUST be at the start of the line
+    # Regex comments can then be placed at the start of lines similar to a powershell script comment starting with #
+    # Character groups in square brackets still respect the spaces
+    $patternWithComments = @'
+    # Look for anything that is NOT an _ or ' or a space
+    [_' ]*
 
+    # Create a capture group looking for any letter A-Z
+    ([A-Z])
+
+    # Make sure there are no dashes or spaces after these matches
+    [^- ]*
+'@
+
+    # Now look how much more readable the above regex is compared to the "normal"
+    # way of writing them like this "[_' ]*([A-Z])[^- ]+"
+
+    return [regex]::Matches($Phrase.ToUpper(), $patternWithComments, 'IgnorePatternWhitespace') |
+        ForEach-Object { $_.Groups[1].Value } |
+        Join-String -Separator ''
 }
