319b558452b394f2646619b7b588f51cc61dba9c
Derek Lomax <derekthecool@gmail.com>
2024-03-22 10:06:00 -0600

    Add another dart ISBN solution using a nice functional railway flow

--- dart/isbn-verifier/lib/isbn_verifier.dart
+++ dart/isbn-verifier/lib/isbn_verifier.dart
@@ -1,22 +1,10 @@
 bool isValid(String isbn) {
-  var numbers = RegExp(r'[0-9Xx]').allMatches(isbn).map((match) {
-    String matchedValue = match.group(0)!;
-    return int.parse(matchedValue.toUpperCase() == 'X' ? '10' : matchedValue);
-  }).toList();
-
-  var list = [for (var i = 10; i > 0; i -= 1) i];
-
-  List<int> isbnSum = [];
-  for (int i = 0; i < numbers.length; i++) {
-    isbnSum.add(numbers[i] * list[i]);
-  }
-
-  print(numbers);
-  print(list);
-  print(isbnSum);
-
-  var totalSum = isbnSum.fold(0, (int x, int y) => x + y);
-  print(totalSum);
-
-  return totalSum % 11 == 0;
+  return RegExp(r'[0-9X]')
+      .allMatches(isbn)
+      .map((match) => int.parse(match.group(0)! == 'X' ? '10' : match.group(0)!))
+      .toList()
+      .asMap()
+      .entries
+      .map((entry) => entry.value * (10 - entry.key))
+      .fold(0, (int sum, int element) => sum + element) % 11 == 0;
 }
