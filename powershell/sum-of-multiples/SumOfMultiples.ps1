f46c208e4e05f58a91eeec0a27c5c5e711de06af
Derek Lomax <derekthecool@gmail.com>
2024-04-28 16:51:55 -0600

    A bit more functional powershell sum of multiples solution

--- powershell/sum-of-multiples/SumOfMultiples.ps1
+++ powershell/sum-of-multiples/SumOfMultiples.ps1
@@ -1,41 +1,20 @@
 Function Get-SumOfMultiples {
-    <#
-    .SYNOPSIS
-    Given a number, find the sum of all the unique multiples of particular numbers up to
-    but not including that number.
-
-    .DESCRIPTION
-    If we list all the natural numbers below 20 that are multiples of 3 or 5,
-    we get 3, 5, 6, 9, 10, 12, 15, and 18.
-
-    .PARAMETER Multiples
-    An array of the factors 
-
-    .PARAMETER Limit
-    The value BELOW which we test for
-
-    .EXAMPLE
-    Get-SumOfMultiples -Multiples @(3, 5) -Limit 10
-    
-    Returns 23
-    #>
     [CmdletBinding()]
     Param(
         [int[]]$Multiples,
         [int]$Limit
     )
 
-    $Global:total = 0
-    Write-Host "Input $Multiples,  $Limit"
-
-    1 .. ($Limit - 1)
+    1..($Limit - 1)
     | Where-Object {
-        $currentNumber = $_
+        $number = $_
         $Multiples
-        | Where-Object { $currentNumber % $_ -eq 0  } | Where-Object { $_ -lt $Limit }
+        | Where-Object { $number % $_ -eq 0 }
+        | Measure-Object
+        | Select-Object -ExpandProperty Count
     }
-    | ForEach-Object { $Global:total += $_ }
-
-    $Global:total
+    | Where-Object { $_ -gt 0 }
+    | Measure-Object -Sum
+    | Select-Object -ExpandProperty Sum
 
 }
