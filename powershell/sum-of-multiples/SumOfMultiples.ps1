98d1787c01b0fa63fbaa522b215cdd2f49d0fa8d
Derek Lomax <derekthecool@gmail.com>
2024-04-28 17:15:40 -0600

    Try Linq approach to solving (not very easy to use)

--- powershell/sum-of-multiples/SumOfMultiples.ps1
+++ powershell/sum-of-multiples/SumOfMultiples.ps1
@@ -4,17 +4,21 @@ Function Get-SumOfMultiples {
         [int[]]$Multiples,
         [int]$Limit
     )
+    # Ensure necessary assembly for LINQ is loaded
+    Add-Type -AssemblyName System.Core
 
-    1..($Limit - 1)
-    | Where-Object {
-        $number = $_
-        $Multiples
-        | Where-Object { $number % $_ -eq 0 }
-        | Measure-Object
-        | Select-Object -ExpandProperty Count
-    }
-    | Where-Object { $_ -gt 0 }
-    | Measure-Object -Sum
-    | Select-Object -ExpandProperty Sum
+    # Use LINQ to create a range
+    $range = [System.Linq.Enumerable]::Range(1, $Limit - 1)
 
+    # Filter the range to only include numbers that are multiples of any numbers in $Multiples
+    $filtered = [System.Linq.Enumerable]::Where(
+        $range,
+        [Func[int, bool]] {
+            param($number)
+            $Multiples | Where-Object { $number % $_ -eq 0 } | Measure-Object | Select-Object -ExpandProperty Count
+        }
+    )
+
+    # Sum the filtered numbers
+    [System.Linq.Enumerable]::Sum($filtered)
 }
