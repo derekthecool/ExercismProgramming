29d2e44ad1407efb32a8b0eafea497d3099a20e3
Derek Lomax <derekthecool@gmail.com>
2024-11-07 11:04:18 -0700

    feat(C): difference of squares

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,29 @@
 #include "difference_of_squares.h"
+#include "test-framework/unity_internals.h"
+
+unsigned int sum_of_squares(unsigned int number)
+{
+    unsigned int result = 0;
+    for (unsigned int i = 1; i <= number; i++)
+    {
+        printf("%s result: %u\n", "sum_of_squares", result);
+        result += i * i;
+    }
+    return result;
+}
+
+unsigned int square_of_sum(unsigned int number)
+{
+    unsigned int result = 0;
+    for (unsigned int i = 1; i <= number; i++)
+    {
+        printf("%s result: %u\n", "square_of_sum", result);
+        result += i;
+    }
+    return result * result;
+}
+
+unsigned int difference_of_squares(unsigned int number)
+{
+    return square_of_sum(number) - sum_of_squares(number);
+}
