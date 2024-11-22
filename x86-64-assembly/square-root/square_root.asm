a519f3f0ef3ee8d289247090e49d31da5bf03f39
Derek Lomax <derekthecool@gmail.com>
2024-11-22 10:52:48 -0700

    feat(square-root-asm): solve square root

--- x86-64-assembly/square-root/square_root.asm
+++ x86-64-assembly/square-root/square_root.asm
@@ -2,6 +2,14 @@ section .text
 global square_root
 square_root:
     ; Provide your implementation here
+    ; Convert integer input (RDI) to double
+    cvtsi2sd xmm0, rdi       ; Convert RDI (integer) to XMM0 (double-precision float)
+
+    ; Compute the square root
+    sqrtsd xmm1, xmm0        ; Calculate the square root of XMM0, store result in XMM1
+
+    ; Convert result back to integer
+    cvttsd2si rax, xmm1      ; Convert XMM1 (double-precision float) to RAX (integer)
     ret
 
 %ifidn __OUTPUT_FORMAT__,elf64
